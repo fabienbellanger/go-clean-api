e97d9feb00bda34ce2b2604fc115d8a1314d002a
Fabien Bellanger <fabien.bellanger@apitic.com>
2025-03-12 15:51:33 +0100

    Improve validation

--- utils/valadator.go
+++ utils/valadator.go
@@ -1,38 +1,27 @@
 package utils
 
 import (
-	"fmt"
-	"log"
-	"strings"
+	"encoding/json"
 
 	"github.com/go-playground/validator/v10"
 )
 
 // ValidatorError represents error validation struct.
 type ValidatorError struct {
-	FailedField string
-	Tag         string
-	Value       string
-}
-
-// TODO: Change
-func (ve *ValidatorError) Error() string {
-	return fmt.Sprintf("Field: %s, Tag: %s, Value: %s", strings.ToLower(ve.FailedField), ve.Tag, ve.Value)
+	FailedField string `json:"failed_field" xml:"failed_field"`
+	Tag         string `json:"tag" xml:"tag"`
+	Value       string `json:"value" xml:"value"`
 }
 
 // ValidatorErrors is a slice of ValidatorError.
 type ValidatorErrors []ValidatorError
 
-// TODO: Change
 func (ve *ValidatorErrors) Error() string {
-	e := ""
-	for i, v := range *ve {
-		if i != 0 {
-			e += "\n"
-		}
-		e += v.Error()
+	b, err := json.Marshal(ve)
+	if err != nil {
+		return "error when marshalling validation errors"
 	}
-	return e
+	return string(b)
 }
 
 // ValidateStruct checks if a struct is valid and returns an array of errors
@@ -59,7 +48,6 @@ func ValidateVar(v any, field, tag string) (errors ValidatorErrors) {
 	errs := validate.Var(v, tag)
 	if errs != nil {
 		for _, err := range errs.(validator.ValidationErrors) {
-			log.Printf("%#v\n", err)
 			errors = append(errors, ValidatorError{
 				FailedField: field,
 				Tag:         err.Tag(),
